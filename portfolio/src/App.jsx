5f5a114ffb38230e7d4cc4b6d005ae9568233250
Jaiden <jaidendiaz7953@gmail.com>
2025-02-19 20:04:33 -0500

    Project Completed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Navbar from "../components/Navbar";
-import IntroSection from "../components/IntroSection";
-import TechStackSection from "../components/TechStackSection";
-import RelevantCourseWorkSection from "../components/RelevantCourseWorkSection";
-import Footer from "../components/footer";
-import ProjectSection from "../components/ProjectSection";
-import ContactForm from "../components/ContactSection";
+import Navbar from "./components/Navbar";
+import IntroSection from "./components/IntroSection";
+import TechStackSection from "./components/TechStackSection";
+import RelevantCourseWorkSection from "./components/RelevantCourseWorkSection";
+import Footer from "./components/footer";
+import ProjectSection from "./components/ProjectSection";
+import ContactForm from "./components/ContactSection";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import InfoSection from "../components/InfoSection";
+import InfoSection from "./components/InfoSection";
 // TO DO ADD CONTACT PAGE
 
 function App() {
